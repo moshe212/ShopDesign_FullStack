ea80fc7e676cd01e1558eeefe3dec9c61e50f86d
unknown <moshe212@gmail.com>
2020-10-14 16:10:25 +0300

    26

--- Client/src/Product.js
+++ Client/src/Product.js
@@ -39,7 +39,7 @@ const Product = (props) => {
 
   console.log("props", props);
   useEffect(() => {
-    const socket = socketIOClient("");
+    const socket = socketIOClient({transports: ['websocket']});
     socket.on("UpdateQuantity", (data) => {
       if (props.id === data.id) {
         props.ChangQuantity(data);
