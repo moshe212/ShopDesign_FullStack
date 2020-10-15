ff6d4ba3fbfc83d03d07c05884e0d4b43882bf96
unknown <moshe212@gmail.com>
2020-10-15 16:01:47 +0300

    31

--- Client/src/Product.js
+++ Client/src/Product.js
@@ -41,7 +41,7 @@ const Product = (props) => {
 
   console.log("props", props);
   useEffect(() => {
-    let socket = socketIOClient("https://localhost",{secure: true});
+    let socket = socketIOClient("",{secure: true});
     // const socket = socketIOClient("https://shopfullstack.herokuapp.com");
     socket.on("UpdateQuantity", (data) => {
       if (props.id === data.id) {
